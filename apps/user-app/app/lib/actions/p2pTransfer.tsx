ada8d765e0e908528b962a3ac168825bdec20a3d
Arinjain111 <arinjain111@gmail.com>
2024-08-31 05:39:46 +0530

    fix

--- apps/user-app/app/lib/actions/p2pTransfer.tsx
+++ apps/user-app/app/lib/actions/p2pTransfer.tsx
@@ -24,7 +24,7 @@ export async function p2pTransfer(to: string, amount: number) {
         }
     }
 
-    await prisma.$transaction(async (tx) =>{
+    await prisma.$transaction(async (tx: any) =>{
         await tx.$queryRaw`SELECT * FROM "Balance" WHERE "userId" = ${Number(from)} FOR UPDATE`;
 
         const fromBalance = await tx.balance.findUnique({
